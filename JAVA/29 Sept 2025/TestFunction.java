13be7f7fe2c882247fa1234041c18ef8572bee6b
Abhishek Alkari <147973324+Leuizack77@users.noreply.github.com>
2025-09-30 12:44:46 +0530

    Add files via upload

---
+++ JAVA/29 Sept 2025/TestFunction.java
@@ -0,0 +1,12 @@
+class TestFunction {
+     public static void main(String args []) {
+         f1();
+     }
+     
+     public static void f1() {
+          System.out.println("This is f1 Func.");
+     }
+}
+
+
+ 
