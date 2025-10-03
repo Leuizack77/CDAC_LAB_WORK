e97a721fdc06e4bdcfd1e1637c1265ed9dc51159
Abhishek Alkari <147973324+Leuizack77@users.noreply.github.com>
2025-10-03 21:06:30 +0530

    Add files via upload

---
+++ JAVA/03 Oct 2025/TestStudent.java
@@ -0,0 +1,14 @@
+import java.util.Date;
+
+public class TestStudent {
+
+	public static void main(String[] args) {
+		
+		Student s1= new Student(101,"abhi",100,99,new Date());
+		System.out.println(s1);
+		
+		Student s2= new Student(102,"amol",100,990,new Date());
+		System.out.println(s2);
+	}
+
+}
---
+++ JAVA/03 Oct 2025/TestVehicle.java
@@ -0,0 +1,13 @@
+import java.util.Date;
+
+public class TestVehicle {
+
+	public static void main(String[] args) {
+		Vehicle v1 = new Vehicle(1,"Abhishek",2, new Date());
+		System.out.println(v1);
+		
+		
+
+	}
+
+}
