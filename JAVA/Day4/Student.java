f9a22293da9162080eabf4f2708ddb6b7c5d4580
Abhishek Alkari <147973324+Leuizack77@users.noreply.github.com>
2025-10-10 20:11:17 +0530

    Student.java

---
+++ JAVA/Day4/Student.java
@@ -0,0 +1,66 @@
+import java.util.Date;
+
+public class Student {
+	
+	private int id;
+	private String name;
+	private float m1;
+	private float m2;
+	private Date dob;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public float getM1() {
+		return m1;
+	}
+	public void setM1(float m1) {
+		this.m1 = m1;
+	}
+	public float getM2() {
+		return m2;
+	}
+	public void setM2(float m2) {
+		this.m2 = m2;
+	}
+	public Date getDob() {
+		return dob;
+	}
+	public void setDob(Date dob) {
+		this.dob = dob;
+	}
+	
+	
+	public Student(int id, String name, float m1, float m2, Date dob) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.m1 = m1;
+		this.m2 = m2;
+		this.dob = dob;
+	}
+	
+	
+	public Student() {
+		this.id = 0;
+		this.name = null;
+		this.m1 = 0.0f;
+		this.m2 = 0.0f;
+		this.dob = null;
+	}
+	
+	
+	@Override
+	public String toString() {
+		return "Student [id=" + id + ", name=" + name + ", m1=" + m1 + ", m2=" + m2 + ", dob=" + dob + "]";
+	}
+}
