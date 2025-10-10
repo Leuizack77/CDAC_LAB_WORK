be05355bdebc794d7f6e0b7a26b15ef8fcb72d1a
Abhishek Alkari <147973324+Leuizack77@users.noreply.github.com>
2025-10-10 19:46:47 +0530

    ClassA.java

---
+++ JAVA/Day7/com.demo.beans/ClassA.java
@@ -0,0 +1,16 @@
+package com.demo.beans;
+
+public class ClassA {
+	private int x;
+	
+	public final void m1() {
+		System.out.println("in m1 of classA");
+	}
+}
+
+ //class class B extends class A {
+	 public void m1() {
+		 System.out.println("in m1 of classA");
+	 }
+	
+}
