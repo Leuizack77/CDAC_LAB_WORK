0b76c7190e79913241f6eda090a3616f89ca13cb
Abhishek Alkari <147973324+Leuizack77@users.noreply.github.com>
2025-10-15 11:54:20 +0530

    EmployeeService.java

---
+++ JAVA/Day11/SetCollectionDemo/src/com.set.service/EmployeeService.java
@@ -0,0 +1,30 @@
+package com.set.service;
+
+import java.util.List;
+import java.util.Set;
+
+import com.set.beans.Employee;
+
+public interface EmployeeService {
+
+	boolean addNewEmployee();
+
+	Set<Employee> displayAll();
+
+	Employee displayById(int id);
+
+	Set<Employee> displayByName(String nm);
+
+	boolean deleteBySal(double sal);
+
+	boolean deleteById(int id);
+
+	boolean updateBySal(int id, double sal);
+
+	List<Employee> sortBySal();
+
+	Set<Employee> sortById();
+
+	List<Employee> sortByName();
+
+}
