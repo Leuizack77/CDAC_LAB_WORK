1673f1ae124190d61b15be590deeb1d7917baeb3
Abhishek Alkari <147973324+Leuizack77@users.noreply.github.com>
2025-10-15 12:22:55 +0530

    Add files via upload

---
+++ JAVA/Day13/ExceptionDemo/src/com.exception.test/TestExceptiondemo3.java
@@ -0,0 +1,19 @@
+package com.exception.test;
+
+import com.exception.exceptions.NegativeSalaryException;
+import com.exception.service.MyService;
+
+public class TestExceptiondemo3 {
+
+	public static void main(String[] args) {
+		MyService ms=new MyService();
+		try {
+			ms.acceptData();
+		} catch (NegativeSalaryException e) {
+			// TODO Auto-generated catch block
+			System.out.println(e.getMessage());
+		}
+
+	}
+
+}
