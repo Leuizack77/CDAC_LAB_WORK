c7cd5a2b0f95acc2980c8fbf394953e6cd22d0e8
Abhishek Alkari <147973324+Leuizack77@users.noreply.github.com>
2025-11-04 14:55:15 +0530

    TestMergeSort.java

--- DSA/Day5_Sorting/src/com/MergeSort/search/TestMergeSort.java
+++ DSA/Day5_Sorting/src/com/MergeSort/search/TestMergeSort.java
@@ -1,11 +1,15 @@
-package com.MergeSort.search;
+package com.MergeSort.test;
 
-public class TestMergeSort {
+import com.MergeSort.sorting.MergerSortAlgorithm;
+
+public class TestMergerSort {
 
 	public static void main(String[] args) {
-		int[] arr= {20,5,7,11,21,2,7,25,30,3};
-		MergeSortAlgorithm.mergeSortAscending(arr, 0,arr.length-1);
-
+		
+		int[] arr = {90,2,1,43,5,65,7,89};
+		int arr1 [] = {20,5,7,11,21,2,7,25,30,3};
+		MergerSortAlgorithm.mergeSortAscending(arr, 0, arr.length - 1);
+		MergerSortAlgorithm.mergeSortAscending(arr1, 0, arr.length - 1);
 	}
 
 }
