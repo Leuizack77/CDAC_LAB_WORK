1dca94ef028a06bde8f60ef5a993b090d7b8d0dd
Abhishek Alkari <147973324+Leuizack77@users.noreply.github.com>
2025-11-04 14:59:31 +0530

    TestInsertionSort.java

--- DSA/Day4/src/com/InsertionSort/search/TestInsertionSort.java
+++ DSA/Day4/src/com/InsertionSort/search/TestInsertionSort.java
@@ -1,11 +1,26 @@
-package com.InsertionSort.search;
+package com.InsertionSort.test;
+
+import com.InsertionSort.sorting.InsertionSortAlgorithm;
 
 public class TestInsertionSort {
 
 	public static void main(String[] args) {
-		int arr[] = {21,11,13,22,25,7,10,8,1};
+		int[] arr = {2,6,21,5,43,33,43};
+		int arr1[] = {21,11,13,22,25,7,10,8,1};
 		
+		System.out.println("Ascending");
 		InsertionSortAlgorithm.insertionSortAscending(arr);
+		System.out.println("======================================");
+		System.out.println("Descending");
 		InsertionSortAlgorithm.insertionSortDescending(arr);
+		System.out.println("======================================");
+		
+		System.out.println("Ascending");
+		InsertionSortAlgorithm.insertionSortAscending(arr1);
+		System.out.println("======================================");
+		System.out.println("Descending");
+		InsertionSortAlgorithm.insertionSortDescending(arr1);
+
 	}
+
 }
