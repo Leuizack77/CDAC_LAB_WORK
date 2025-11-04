11041131fc2c368aca70d8cbcd532c4d8100381d
Abhishek Alkari <147973324+Leuizack77@users.noreply.github.com>
2025-11-04 15:01:35 +0530

    TestQuickSort.java

--- DSA/Day4/src/com/QuickSort/search/TestQuickSort.java
+++ DSA/Day4/src/com/QuickSort/search/TestQuickSort.java
@@ -1,10 +1,18 @@
-package com.QuickSort.search;
+package com.QuickSort.test;
 
-import com.QuickSort.search.QuickSortAlgorithm;
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+import com.QuickSort.sorting.QuickSortAlgorithm;
 
 public class TestQuickSort {
+
 	public static void main(String[] args) {
-		int[] arr= {5,3,8,1,4,6,2,7};
-		QuickSortAlgorithm.quicksort(arr, 0, arr.length-1);
+		int[] arr = {54,43,333,231,115,208,327,324};
+		int arr1[] = {5,3,8,1,4,6,2,7};
+		
+	 QuickSortAlgorithm.quicksort(arr, 0, arr.length - 1);
+	 System.out.println("============================================");
+	 QuickSortAlgorithm.quicksort(arr1, 0, arr.length - 1);
 	}
 }
