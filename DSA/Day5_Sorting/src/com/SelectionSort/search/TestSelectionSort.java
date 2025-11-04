46bdab59598ff82e0575747bed2c1bac1e6800f2
Abhishek Alkari <147973324+Leuizack77@users.noreply.github.com>
2025-11-04 14:56:44 +0530

    TestSelectionSort.java

--- DSA/Day5_Sorting/src/com/SelectionSort/search/TestSelectionSort.java
+++ DSA/Day5_Sorting/src/com/SelectionSort/search/TestSelectionSort.java
@@ -1,12 +1,17 @@
-package com.SelectionSort.search;
+package com.SelectionSort.test;
+
+import com.SelectionSort.sorting.SelectionSortAlgorithm;
 
 public class TestSelectionSort {
 
 	public static void main(String[] args) {
 		int[] arr= {15,13,30,20,5,7,21,2};
+		System.out.println("Ascending Selection Sort => ");
 		SelectionSortAlgorithm.selectionSortAscending(arr);
-		System.out.println("------------------------------------------");
+		
+		System.out.println("=============================================");
+		
+		System.out.println("Descending Selection Sort => ");
 		SelectionSortAlgorithm.selectionSortDescending(arr);
 	}
-
 }
