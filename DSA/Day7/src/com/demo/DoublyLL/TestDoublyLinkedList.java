0536a2e5d6917fa0a46e6f0ae8c16ef49a039c36
spacesunneptune <spacesunneptune@protonmail.com>
2025-11-06 16:41:49 +0530

    Added

---
+++ DSA/Day7/src/com/demo/DoublyLL/TestDoublyLinkedList.java
@@ -0,0 +1,14 @@
+package com.demo.DoublyLL;
+
+public class TestDoublyLinkedList {
+	
+	public static void main(String args[]) {
+		DoublyLinkedList dlist=new DoublyLinkedList();
+		dlist.addNode(12);
+		dlist.displayData();
+		dlist.addNode(15);
+		dlist.displayData();
+		dlist.addNode(13);
+		dlist.displayData();
+	}
+}
